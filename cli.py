a180bf501faf4ab67c2c19e225eb1137ec0982bb
Fernando Farias <fernnf@gmail.com>
2017-02-17 19:25:49 -0300

    Creating repository2

--- cli.py
+++ cli.py
@@ -18,7 +18,7 @@ def cli():
 @click.option('-d', '--description', nargs=1, required=True,
               type=click.STRING, help='The evaluation description e.g (case1 or kernel 4.9)')
 @click.option('-b', '--database', type=click.STRING,
-              nargs=3, help='The database configuration e.g: 127.0.0.1 user password', required=True)
+              nargs=3, help='The database configuration e.g: 127.0.0.1 user password')
 def cmd_eval(operating_system, type_evaluation,
              lua_trafficgen_dir, description, databas):
     pass
